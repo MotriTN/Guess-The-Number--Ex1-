069727d4ef64f9edf066ed6aff39d00204d248ae
MotriTN <gamerMT95@gmail.com>
2022-10-01 07:13:31 +0100

    Without Def

--- code.py
+++ code.py
@@ -2,14 +2,6 @@
 from random import*
 N=randint(1,30)
 i=0
-def ret3i():
-    P=int(input("Guess a number between 1 and 30: "))
-    if N==P:
-        print("Good Job!")
-    elif N<P:
-        print("Go Less!")
-    else:
-        ("Go More!")
 #Code
 while i<5:
     P=int(input("Guess a number between 1 and 30: "))
@@ -19,10 +11,6 @@ while i<5:
     elif N<P:
         i=i+1
         print("Go Less!")
-        ret3i()
     else:
         i=i+1
         ("Go More!")
-        ret3i()
-
-    
